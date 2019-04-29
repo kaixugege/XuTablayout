2b16120d9eab8bb189181ea15cdce94c3e74c116
kaixugege <604312074@qq.com>
2019-04-29 17:22:29 +0800

    add a Test class.

---
+++ xu_lib_tablayout/src/main/java/com/xugege/xu_lib_tablayout/Test.java
@@ -0,0 +1,15 @@
+package com.xugege.xu_lib_tablayout;
+
+/**
+ * @Author: KaixuGege
+ * Time:           2019/4/29
+ * ProjectName:    XuTablayout
+ * ClassName:
+ * Info:
+ */
+public class Test {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
