9f7f9fc5b571898a8ae9a7018daba4169ff1632d
kaixugege <604312074@qq.com>
2019-04-30 11:03:26 +0800

    Add a tablout lib.

--- xu_lib_tablayout/src/main/java/com/xugege/xu_lib_tablayout/Test.java
+++ xu_lib_tablayout/src/main/java/com/xugege/xu_lib_tablayout/Test.java
@@ -12,4 +12,5 @@ public class Test {
     public int add(int a, int b) {
         return a + b;
     }
+
 }
---
+++ xu_lib_tablayout/src/main/java/com/xugege/xu_lib_tablayout/tablayout/listener/OnTabSelectListener.java
@@ -0,0 +1,6 @@
+package com.xugege.xu_lib_tablayout.tablayout.listener;
+
+public interface OnTabSelectListener {
+    void onTabSelect(int position);
+    void onTabReselect(int position);
+}
