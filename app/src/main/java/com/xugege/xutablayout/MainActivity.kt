5f0c5d0be7046eabdaf818c5ae94255448028b87
kaixu <604312074@qq.com>
2019-05-21 22:28:30 +0800

    给tab添加了一个touch事件

--- app/src/main/java/com/xugege/xutablayout/MainActivity.kt
+++ app/src/main/java/com/xugege/xutablayout/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.xugege.xutablayout
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.xugege.xutablayout.delegate.BlankFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BlankFragment.OnFragmentInteractionListener {
+    override fun onFragmentInteraction(uri: Uri) {
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
